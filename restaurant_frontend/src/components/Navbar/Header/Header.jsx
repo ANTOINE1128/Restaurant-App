ca90aa4eaf8d31dd858fef3ecfe74ca575a024de
Antoine Makdessy <makdessyantoine@gmail.com>
2024-07-21 23:30:38 +0300

    Create  components ,assets , pages

---
+++ restaurant_frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Order Your Favorite Food Here</h2>
+        <p>Choose from a diverse menu featuring a delecatable array of dishes crafted with love, and the best ingriendetns to satisfy your cravings and elevate your dining experience, one delicious meal at a time </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
